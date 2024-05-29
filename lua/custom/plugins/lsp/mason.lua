cfb290aeef0eb207222b6d28eaaea16169ebabfc
William Munoz <william@rustdeveloper.io>
2024-05-29 14:38:18 -0500

    remove lsp servers not installed

--- lua/custom/plugins/lsp/mason.lua
+++ lua/custom/plugins/lsp/mason.lua
@@ -27,27 +27,28 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        "pyright",
+        --"rust-analyzer",
+        --"tsserver",
+        --"html",
+        --"cssls",
+        --"tailwindcss",
+        --"svelte",
+        --"lua_ls",
+        --"graphql",
+        --"emmet_ls",
+        --"prismals",
+        --"pyright",
       },
     })
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier", -- prettier formatter
+        --"prettier", -- prettier formatter
         "stylua", -- lua formatter
         "isort", -- python formatter
         "black", -- python formatter
         "pylint",
-        "eslint_d",
+        --"eslint_d",
       },
     })
   end,
