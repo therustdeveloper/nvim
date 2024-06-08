dd1b3c5a600822e2477e84b279a83117be07ac65
William Munoz <william@rustdeveloper.io>
2024-06-07 22:00:40 -0500

    add window title according to the file open

--- lua/custom/core/options.lua
+++ lua/custom/core/options.lua
@@ -37,3 +37,6 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+-- allows vim to set the terminal window title according to the current file name
+opt.title = true
