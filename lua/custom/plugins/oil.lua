de43b815dda71e45603a6b788ef7098cb18247a0
William Munoz <william@rustdeveloper.io>
2024-06-14 19:09:26 -0500

    configuring float window
    configuring float window in oil plugin

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -15,6 +15,23 @@ return {
       columns = {
         "icon",
       },
+
+      -- Configuration for the floating window in oil.open_float
+      float = {
+        -- Padding around the floating window
+        padding = 2,
+        max_width = 0,
+        max_height = 0,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+        -- This is the config that will be passed to nvim_open_win.
+        -- Change values here to customize the layout
+        override = function(conf)
+          return conf
+        end,
+      },
     })
 
     -- set keymaps
