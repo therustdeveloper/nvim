d8893390eed1eb869e9d08048d76c575ff69182c
William Munoz <william@rustdeveloper.io>
2024-06-14 19:25:44 -0500

    remove the float from oil
    this is probably adding an issue with rust

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -15,23 +15,6 @@ return {
       columns = {
         "icon",
       },
-
-      -- Configuration for the floating window in oil.open_float
-      float = {
-        -- Padding around the floating window
-        padding = 2,
-        max_width = 0,
-        max_height = 0,
-        border = "rounded",
-        win_options = {
-          winblend = 0,
-        },
-        -- This is the config that will be passed to nvim_open_win.
-        -- Change values here to customize the layout
-        override = function(conf)
-          return conf
-        end,
-      },
     })
 
     -- set keymaps
