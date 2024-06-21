31258847d998ba8628303e2571640b1efa012c2a
William Munoz <william@rustdeveloper.io>
2024-06-21 16:52:51 -0500

    configuring buffers
    buffers should work with tabs

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -2,9 +2,31 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-    },
-  },
+  --opts = {
+  --  options = {
+  --    mode = "tabs",
+  --  },
+  --},
+
+  config = function()
+    local bufferline = require("bufferline")
+
+    bufferline.setup({
+      options = {
+        mode = "tabs",
+        style_preset = bufferline.style_preset.default,
+        themable = true,
+        numbers = "ordinal",
+        indicator = {
+          icon = " ",
+          style = "icon",
+        },
+        buffer_close_icon = "󰅖",
+        modified_icon = "●",
+        close_icon = "",
+        left_trunc_marker = "",
+        right_trunc_marker = "",
+      },
+    })
+  end,
 }
