e7be6e04349decd9dcb7e1297422feaf8b885d13
William Munoz <william@rustdeveloper.io>
2024-07-07 14:11:08 -0500

    add battery
    battery in neovim

---
+++ lua/custom/plugins/battery.lua
@@ -0,0 +1,17 @@
+return {
+  "justinhj/battery.nvim",
+
+  config = function()
+    local battery = require("battery")
+
+    battery.setup({
+      update_rate_seconds = 30,
+      show_status_when_no_battery = true,
+      show_plugged_icon = true,
+      show_unplugged_icon = true,
+      show_percent = true,
+      vertical_icons = true,
+      multiple_battery_selection = 1,
+    })
+  end,
+}
