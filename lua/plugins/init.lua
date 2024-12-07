06d20338e656ec5e9f628475f5f40baa9fd1594e
William Munoz <william@godeveloper.io>
2024-12-06 22:04:18 -0500

    add noice plugin
    nice ui for vim commands

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -133,4 +133,14 @@ return {
     end,
   },
 
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {},
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    }
+  },
+
 }
