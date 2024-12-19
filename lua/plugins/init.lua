345de60bb77fd89ca94cb6bb2f6a97a1afe74ca1
William Munoz <william@godeveloper.io>
2024-12-18 23:00:53 -0500

    add terminal

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -143,4 +143,17 @@ return {
     }
   },
 
+  -- lazy.nvim
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      terminal = {
+        -- your terminal configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    }
+  },
+
 }
