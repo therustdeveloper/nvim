94a7d18384647c71fce565dba8e012446bf0d5a7
William Munoz <william@rustdeveloper.io>
2025-02-09 12:53:47 -0500

    removing double hint in rust

--- lua/custom/plugins/inlay-hints.lua
+++ lua/custom/plugins/inlay-hints.lua
@@ -1,8 +1,17 @@
 return {
-    "MysticalDevil/inlay-hints.nvim",
-    event = "LspAttach",
-    dependencies = { "neovim/nvim-lspconfig" },
-    config = function()
-        require("inlay-hints").setup()
-    end
+  "MysticalDevil/inlay-hints.nvim",
+  event = "LspAttach",
+  dependencies = { "neovim/nvim-lspconfig" },
+  config = function()
+    require("inlay-hints").setup({
+      inlay_hints = {
+        type_hints = {
+          show = true,
+          prefix = "",
+          remove_redundant = true,
+        },
+      },
+      renderer = "inlay-hints/render/simple",
+    })
+  end,
 }
