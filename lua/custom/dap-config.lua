6d5f56569e8812af0570c627e4a7d903395bb1a6
William Munoz <william@rustdeveloper.io>
2025-02-25 08:19:32 -0500

    detect the executable name automatically in dap config for rust

--- lua/custom/dap-config.lua
+++ lua/custom/dap-config.lua
@@ -17,7 +17,28 @@ dap.configurations.rust = {
     type = "codelldb",
     request = "launch",
     program = function()
-      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+      -- return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+      -- Automatically detect the executable from the Cargo.toml file
+      local cargo_toml = vim.fn.findfile("Cargo.toml", vim.fn.getcwd() .. ";")
+      if cargo_toml == "" then
+        return vim.notify("Cargo.toml not found", vim.log.levels.ERROR)
+      end
+
+      -- Read the package name from Cargo.toml
+      local package_name = nil
+      for line in io.lines(cargo_toml) do
+        if line:match("^name%s*=%s*") then
+          package_name = line:match("^name%s*=%s*[\"']?([^\"']+)[\"']?")
+          break
+        end
+      end
+
+      if not package_name then
+        return vim.notify("Could not determine package name from Cargo.toml", vim.log.levels.ERROR)
+      end
+
+      -- Construct the path to the debug executable
+      return vim.fn.getcwd() .. "/target/debug/" .. package_name
     end,
     cwd = "${workspaceFolder}",
     stopOnEntry = false,
