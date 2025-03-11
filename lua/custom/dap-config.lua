6cde71041212b9ef6115c72b42c4db54e954a614
William Munoz <william@rustdeveloper.io>
2025-03-11 07:27:36 -0500

    move keymaps to dap-config

--- lua/custom/dap-config.lua
+++ lua/custom/dap-config.lua
@@ -1,4 +1,5 @@
 local dap = require("dap")
+local keymap = vim.keymap
 
 -- Adapter configuration for codelldb (Rust)
 dap.adapters.codelldb = {
@@ -93,3 +94,18 @@ dap.configurations.go = {
     port = 2345,
   },
 }
+
+-- Nvim DAP
+keymap.set("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
+keymap.set("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
+keymap.set("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
+keymap.set("n", "<Leader>dc", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
+keymap.set("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
+keymap.set(
+  "n",
+  "<Leader>dd",
+  "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+  { desc = "Debugger set conditional breakpoint" }
+)
+keymap.set("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
+keymap.set("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
