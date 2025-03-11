5a9041b361ba95c56d93fa04ccee027f3dad7739
William Munoz <william@rustdeveloper.io>
2025-03-10 23:47:41 -0500

    return the nvim ui to normal after dap debug

--- lua/custom/plugins/dap-ui.lua
+++ lua/custom/plugins/dap-ui.lua
@@ -2,9 +2,48 @@ return {
   "rcarriga/nvim-dap-ui",
   dependencies = {
     "mfussenegger/nvim-dap",
+    "nvim-tree/nvim-tree.lua",
     "nvim-neotest/nvim-nio",
   },
   config = function()
-    require("dapui").setup()
+    local dap, dapui = require("dap"), require("dapui")
+    local nvim_tree_api = require("nvim-tree.api")
+
+    dapui.setup()
+
+    -- Store initial window layout
+    local initial_layout = nil
+
+    local function capture_layout()
+      initial_layout = vim.fn.winlayout()
+    end
+
+    local function restore_layout()
+      if initial_layout then
+        vim.cmd("NvimTreeClose") -- Close NvimTree before resetting
+        vim.cmd("tabnew") -- Open a temporary tab
+        vim.cmd("tabclose") -- Close it to reset splits
+        vim.cmd("NvimTreeOpen") -- Reopen NvimTree
+
+        -- Restore window layout manually (Neovim doesn't support restoring layouts directly)
+        -- You might need to manually configure splits here based on your preferences.
+        vim.cmd("wincmd =") -- Balance windows to match previous layout
+      end
+    end
+
+    -- Capture layout before dap-ui opens
+    dap.listeners.before.event_initialized["dapui_config"] = function()
+      capture_layout()
+      dapui.open()
+    end
+
+    -- Restore layout after dap-ui closes
+    local function close_dapui_and_restore()
+      dapui.close()
+      vim.defer_fn(restore_layout, 100) -- Slight delay for proper execution
+    end
+
+    dap.listeners.before.event_terminated["dapui_config"] = close_dapui_and_restore
+    dap.listeners.before.event_exited["dapui_config"] = close_dapui_and_restore
   end,
 }
