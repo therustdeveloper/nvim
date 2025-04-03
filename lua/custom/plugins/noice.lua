216753c3b2a6c347e3b9651075b0551c4858584e
William Munoz <william@rustdeveloper.io>
2025-04-03 00:39:10 -0500

    implement npm package info

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -16,6 +16,7 @@ return {
       "rcarriga/nvim-notify",
       opts = {
         background_colour = "#000000",
+        timeout = 2000,
       },
     },
   },
--- lua/custom/plugins/npm-package-info.lua
+++ lua/custom/plugins/npm-package-info.lua
@@ -4,7 +4,16 @@ return {
   ft = { "json" },
   config = function(_, opts)
     local package_info = require("package-info")
-    package_info.setup(opts)
-    package_info.show()
+    package_info.setup({
+      auto_reload = false, -- Disables automatic refreshing
+      hide_up_to_date = false, -- Hides packages that are already up to date
+      notification_window = { -- Try setting this to `nil` to prevent popups
+        border = "none",
+        position = "top",
+      },
+    })
+    -- vim.api.nvim_create_user_command("PackageInfo", function()
+    --  package_info.show()
+    -- end, {})
   end,
 }
